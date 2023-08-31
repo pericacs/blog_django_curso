7f865a30447f57ad5b4ca25762b2b5f9a718565e
pericacs <pericacs@gmail.com>
2023-08-31 11:35:25 -0300

    atualizando o git

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'site_setup',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'site_setup.context_processors.context_processor_example',
+                'site_setup.context_processors.site_setup',
             ],
         },
     },
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,6 +3,5 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 wait_psql.sh
 collectstatic.sh
-makemigrations.sh
 migrate.sh
 runserver.sh
