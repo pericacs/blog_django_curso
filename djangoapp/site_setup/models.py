dffadcbcb462643409cdee896ecaf69f4703294d
pericacs <pericacs@gmail.com>
2023-09-05 17:10:00 -0300

    colocando os arqvuiso atualizados no git

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -10,7 +10,7 @@ class MenuLink(models.Model):
     url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
     site_setup = models.ForeignKey('SiteSetup', on_delete=models.CASCADE, blank=True, null=True,
-                                   default=None)
+                                   default=None, related_name='menu')
     
     def __str__(self):
         return self.text
